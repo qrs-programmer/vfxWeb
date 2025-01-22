02a2bf647e00113f2bc7ace70ed171c68b7e8bf1
qrs-programmer <166080407+qrs-programmer@users.noreply.github.com>
2025-01-22 13:50:44 -0500

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,6 @@ const app = express();
 const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
-const csv = require('csv-parser');
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -20,17 +19,6 @@ app.get('/works', (req, res) => {
     res.render('works', { page: 'works' });
 });
 
-// Fetch Video Data
-app.get('/videos', (req, res) => {
-    const results = [];
-    fs.createReadStream(path.join(__dirname, 'data', 'video_data.csv'))
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        res.json(results); // Send parsed data as json to client
-    });
-
-});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
